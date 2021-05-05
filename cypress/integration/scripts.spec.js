f635e47444f6ebc03ab74d7a65bd0746109d60d2
Alexander Holmstrom <alexholmstrom.se>
2021-05-05 14:38:37 +0200

    updated scripts

--- cypress/integration/scripts.spec.js
+++ cypress/integration/scripts.spec.js
@@ -4,9 +4,25 @@
 describe('Test suite with PO - FalsktAlarm', () => {
     beforeEach('Visit website', () => {
         cy.visit('/')
+        cy.wait(2000)
         cy.get('.site-title > a').should("contain", "Site Title")
+        
     })
     it('Click ABOUT', () => {
         cy.get('.page-item-2 > a').click().should("contain", "About")
+        cy.wait(2000)
     })
+    it('Click CONTACT', () => {
+        cy.wait(2000)
+        cy.get('.page-item-3 > a').click()
+        cy.wait(2000)
+        cy.title().should('contain','Site Title')
+        cy.contains('Contact')
+        cy.log('Asserted that the word exists on this specific page')
+           
+        
+    })
+
+
+
 });
